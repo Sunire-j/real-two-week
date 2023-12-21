d7f629ce7272b635806107667e2b78843e0388d9
Sunire <81051472+Sunire-j@users.noreply.github.com>
2023-12-21 17:50:54 +0900

    회사컴 데이터

---
+++ sql/member.sql
@@ -0,0 +1,2 @@
+INSERT INTO fortwoweek.member (name, email, provider, nickname, userpwd, isadmin) VALUES ('조석훈', 'sunire@kakao.com', 'naver', null, null, 0);
+INSERT INTO fortwoweek.member (name, email, provider, nickname, userpwd, isadmin) VALUES ('조석훈 (스니레)', 'kastic1537@gmail.com', 'google', null, null, 1);
