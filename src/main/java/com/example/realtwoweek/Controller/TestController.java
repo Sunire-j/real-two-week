917ae45e43d1ab3bfbffc865f24d07f3196adb63
Sunire <81051472+Sunire-j@users.noreply.github.com>
2024-02-02 13:52:17 +0900

    get-username

---
+++ src/main/java/com/example/realtwoweek/Controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.realtwoweek.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    private String getUserName(int member_id){
+        String name ="";
+        //로직
+        return name;
+    }
+}
